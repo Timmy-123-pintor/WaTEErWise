8fc88c82fb66d33db937aaf4f171c3363b43eedd
Timmy John Pintor <timmyjohn.pintor.20@usjr.edu.ph>
2023-11-15 10:07:49 +0800

    Added API End Point
    End Point added and updated the backend. GET realtime data from firebase to API end point

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,21 @@
 import express from 'express';
 import cors from 'cors'; 
 import { connect as connectMQTT } from './services/mqtt/mqttService.js';
-
+import apiEndPoint from '../backend/services/API/apiEndPoint.js'
 const app = express();
 
-// app.use(cors({
-//     origin: ['http://localhost:3000', 'http://10.0.2.2:3000', 'http://localhost:53560']
-// }));
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', apiEndPoint);
 
-// Import the routes
 import customerRoutes from './routes/customer/customerRoutes.js';
 import adminRoutes from './routes/admin/adminRoutes.js';
 import deviceRoutes from './routes/device/deviceRoutes.js';
 import userRoutes from './routes/user/userRoutes.js';
 
-// Middleware for parsing JSON bodies
-app.use(express.json());
+
+app.use('/api', apiEndPoint);
 
 // Use the routes
 app.use('/customer', customerRoutes);
@@ -26,7 +25,7 @@ app.use('/user', userRoutes);
 
 const port = process.env.PORT || 3000;
 
-// Connect to MQTT Broker
+
 connectMQTT();
 
 app.listen(port, '0.0.0.0', () => console.log(`Server is listening on port ${port}`));
